d57b1a7cf253d4a514be3ba1324ce4bf70051eb0
Jonathan Potter <jpotterm@gmail.com>
2017-10-12 11:25:50 -0400

    Take port as argument since we switched one server to 888

--- lib/ssh.py
+++ lib/ssh.py
@@ -1,11 +1,11 @@
 import paramiko
 
 
-def connect(user, host, password=None):
+def connect(user, host, port=22, password=None):
     '''
     Return an ssh connection to `user`@`host`.
     '''
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host, username=user, password=password)
+    client.connect(host, port=port, username=user, password=password)
     return client
